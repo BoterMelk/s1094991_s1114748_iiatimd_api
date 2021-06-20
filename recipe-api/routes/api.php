46fc74a3e4f3286914630a70578f0a95e6bacb4a
Loise van Dijk <loisedijk@gmail.com>
2021-06-20 15:44:45 +0200

    First setup Sanctum

--- recipe-api/routes/api.php
+++ recipe-api/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
+
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
